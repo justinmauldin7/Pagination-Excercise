3802f848f4cda63bd8fd6f9eb5693d842f5b3539
Justin Mauldin <justin.mauldin7@gmail.com>
2022-06-21 15:47:32 -0600

    Updates the "DogsController" "displays recent dogs" test to create 10 dogs instead of just 2.

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe DogsController, type: :controller do
   describe '#index' do
     it 'displays recent dogs' do
-      2.times { create(:dog) }
+      10.times { create(:dog) }
       get :index
       expect(assigns(:dogs).size).to eq(2)
     end
