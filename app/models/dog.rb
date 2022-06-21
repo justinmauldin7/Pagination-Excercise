51a2796fa06060370ec272f18ce8b614ef6e9e30
Justin Mauldin <justin.mauldin7@gmail.com>
2022-06-21 16:46:07 -0600

    Adds the "all_dogs_paginated" scoped method to the dog modal.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,5 @@
 class Dog < ApplicationRecord
   has_many_attached :images
+  scope :all_dogs_paginated, ->(records_per_page, page) { limit(records_per_page)
+                                                          .offset((page - 1) * records_per_page) }
 end
