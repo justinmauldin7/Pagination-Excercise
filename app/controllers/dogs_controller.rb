82c8e72808530cc3609c67a65248212220e5dc44
Justin Mauldin <justin.mauldin7@gmail.com>
2022-06-21 16:41:14 -0600

    Adds the new "set_page" method as a before_action on the index method only on the dogs controller.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,6 @@
 class DogsController < ApplicationController
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
+  before_action :set_page, only: [:index]
 
   # GET /dogs
   # GET /dogs.json
