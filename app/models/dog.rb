dddf89cda3b87cb7a3b8566b84af985fdcf1f478
Justin Mauldin <justin.mauldin7@gmail.com>
2022-06-21 16:46:37 -0600

    Adds the code comment for the new "all_dogs_paginated" scoped method on the dog modal.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,7 @@
 class Dog < ApplicationRecord
   has_many_attached :images
+  # This scoped method will return the correct chunk of dog records depending on
+  # the page & number of records to display per page we pass to this ActiveRecord query.
   scope :all_dogs_paginated, ->(records_per_page, page) { limit(records_per_page)
                                                           .offset((page - 1) * records_per_page) }
 end
